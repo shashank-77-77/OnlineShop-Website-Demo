50a4b0d568ecaaf5508a6c671f93af4ff7194ff6
shashank-77-77 <shashankmankar412@gmail.com>
2025-11-17 22:43:42 +0530

    updates on website

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,8 +4,8 @@ import "../styles/Home.css";
 const Home = () => {
   return (
     <div className="page-container">
-      <h1>Welcome to ShopEase</h1>
-      <p>Discover amazing products and deals!</p>
+      <h1>Welcome to ShopEase Website</h1>
+      <p>Discover amazing products and deals..!</p>
     </div>
   );
 };
